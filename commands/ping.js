6c7d68247ffbc9a4ff61c0bc04cad3ee936b1c49
Hifumindesu <40629881+Hifumindesu@users.noreply.github.com>
2021-10-05 16:02:40 +0800

    Initialize bot

---
+++ commands/ping.js
@@ -0,0 +1,17 @@
+// Check bot response delay
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Checks the bot\'s ping.'),
+	async execute(interaction) {
+		try {
+			const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+			interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+		}
+		catch (error) {
+			console.log(error);
+		}
+	},
+};
---
+++ index.js
@@ -0,0 +1,39 @@
+// Requires
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+
+// Config dotenv
+dotenv.config();
+const token = process.env.TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Command files
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// Event files
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Login to Discord with your client's token
+client.login(token);
